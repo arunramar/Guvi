f1c5daf4c0727af9387d0abefa4eaff728243fcc
Arunkumar Pandyan Ramar <arunramar@yahoo.com>
2022-05-18 14:43:35 +0530

    Add files via upload

---
+++ Day4Tasks/day4.js
@@ -0,0 +1,7 @@
+
+
+var obj1 = { name: "Person 1", age:5 };
+var obj2 = { age:5, name: "Person 1" };
+if (obj1.name == obj2.name && obj1.age == obj2.age){
+    console.log("The objects are equal");
+}else console.log("The objects are not equal");
